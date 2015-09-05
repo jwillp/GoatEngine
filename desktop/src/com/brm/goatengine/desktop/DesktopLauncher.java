9043e00a6f22bb363f190b17e76f056d860ee431
fireraccoon <willyjames.music@gmail.com>
2015-09-05 13:05:11 -0400

    Initial Commit

---
+++ desktop/src/com/brm/goatengine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brm.goatengine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brm.goatengine.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
