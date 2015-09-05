106f4c2ae8c45ffcfb93dd270e2fc2e38219ac98
fireraccoon <willyjames.music@gmail.com>
2015-09-05 13:17:14 -0400

    Basic Graphics Engine

---
+++ core/src/com/brm/goatengine/GraphicsEngine.java
@@ -0,0 +1,72 @@
+package com.brm.goatengine;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * Graphics Engine abstracting Low level Calls
+ * WARNING: This should only be used by the rendering thread
+ */
+public class GraphicsEngine {
+
+    /**
+     * Delta time between frames
+     * @return the time between the last frames
+     */
+    public float getDeltaTime(){
+        return Gdx.graphics.getDeltaTime();
+    }
+
+    /**
+     * Number of frames per second
+     * @return
+     */
+    public float getFPS(){
+        return Gdx.graphics.getFramesPerSecond();
+    }
+
+    /**
+     * Returns the screen Width
+     * @return
+     */
+    public int getScreenWidth(){
+        return Gdx.graphics.getWidth();
+    }
+
+    /**
+     * Returns the screen Width
+     * @return
+     */
+    public int getScreenHeight(){
+        return Gdx.graphics.getHeight();
+    }
+
+
+    public void clearScreen(){
+        Gdx.gl.glClearColor(0,0,0,0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+    }
+
+
+    public void beginrender(){
+        // TODO Spritebatch call
+    }
+
+
+    public void endRender(){
+        // TODO spritebatch call
+    }
+
+
+
+    public boolean isFullScreen(){
+        return Gdx.graphics.isFullscreen();
+    }
+
+    public void setFullScreen(boolean fullScreen){
+
+    }
+
+
+
+}
--- desktop/src/com/brm/goatengine/desktop/DesktopLauncher.java
+++ desktop/src/com/brm/goatengine/desktop/DesktopLauncher.java
@@ -6,7 +6,13 @@ import com.brm.goatengine.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+
+		cfg.title = "GOAT ENGINE";
+		cfg.width = 1920;
+		cfg.height = 1080;
+		cfg.fullscreen = false;
+
+		new LwjglApplication(new Game(), cfg);
 	}
 }
