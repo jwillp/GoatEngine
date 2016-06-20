9887670ddb16b4cb59ad2bede59e071d7d830bb5
fireraccoon <willyjames.music@gmail.com>
2016-06-19 23:44:14 -0400

    Make LuaStateMachine methods not mandatory

--- core/src/com/goatgames/GoatEngine/ScriptingEngine/lua/LuaMachineState.java
+++ core/src/com/goatgames/GoatEngine/ScriptingEngine/lua/LuaMachineState.java
@@ -5,6 +5,7 @@ import com.goatgames.goatengine.fsm.MachineState;
 import com.goatgames.goatengine.input.events.InputEvent;
 import com.goatgames.goatengine.physics.CollisionEvent;
 import org.luaj.vm2.LuaTable;
+import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 
 /**
@@ -20,33 +21,45 @@ public class LuaMachineState extends MachineState{
 
     @Override
     public void onEnter(String oldStateName, MachineState oldState) {
-        this.state.get("onEnter").call(CoerceJavaToLua.coerce(oldStateName),
-                CoerceJavaToLua.coerce(oldState));
+        LuaValue method = this.state.get("onEnter");
+        if(method != LuaValue.NIL)
+            method.call(CoerceJavaToLua.coerce(oldStateName),
+                    CoerceJavaToLua.coerce(oldState));
     }
 
     @Override
     public void onExit(String newStateName, MachineState newState) {
-        this.state.get("onExit").call(CoerceJavaToLua.coerce(newStateName),
-                CoerceJavaToLua.coerce(newState));
+        LuaValue method = this.state.get("onExit");
+        if(method != LuaValue.NIL)
+            method.call(CoerceJavaToLua.coerce(newStateName),
+                    CoerceJavaToLua.coerce(newState));
     }
 
     @Override
     public void onCollisionEvent(CollisionEvent event) {
-        this.state.get("onCollisionEvent").call(CoerceJavaToLua.coerce(event));
+        LuaValue method = this.state.get("onCollisionEvent");
+        if(method != LuaValue.NIL)
+            method.call(CoerceJavaToLua.coerce(event));
     }
 
     @Override
     public void onGameEvent(GameEvent event) {
-        this.state.get("onGameEvent").call(CoerceJavaToLua.coerce(event));
+        LuaValue method = this.state.get("onGameEvent");
+        if(method != LuaValue.NIL)
+            method.call(CoerceJavaToLua.coerce(event));
     }
 
     @Override
     public void onInputEvent(InputEvent event) {
-        this.state.get("onInputEvent").call(CoerceJavaToLua.coerce(event));
+        LuaValue method = this.state.get("onInputEvent");
+        if(method != LuaValue.NIL)
+            method.call(CoerceJavaToLua.coerce(event));
     }
 
     @Override
     public void update(float dt) {
-        this.state.get("update").call(CoerceJavaToLua.coerce(dt));
+        LuaValue method = this.state.get("update");
+        if(method != LuaValue.NIL)
+            method.call(CoerceJavaToLua.coerce(dt));
     }
 }
